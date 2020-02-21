cd4b1907c8d0518f347c1bc1aca39b2129fc85d7
Robert Shields <robert.shieldsiv@gmail.com>
2020-02-21 22:02:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament) FROM bears GROUP BY temperament ORDER BY COUNT(*) DESC LIMIT 1;"
+  "SELECT COUNT(temperament) FROM bears GROUP BY temperament ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def selects_bear_that_killed_Tim
