b6721f1d2e720aa4c3bb3ecc8b0623790b77b6e4
Robert Shields <robert.shieldsiv@gmail.com>
2020-02-21 22:07:10 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Cholocate", 22, "Male", "Brown", "Calm", 1),
+("Rowdy", 7, "Male", "Black", "Moody", 0),
+("Tabitha", 14, "Female", "White", "Aggressive", 1),
+("Sergeant Brown", 6, "Male", "Brown", "Moody", 0),
+("Melissa", 2, "Female", "Black", "Loner", 0),
+("Grinch", 54, "Male", "Brown", "Aggressive", 1),
+("Wendy", 19, "Female", "White", "Calm", 1),
+(NULL, 99, "Male", "Gray", "Unpredictable", 0);
